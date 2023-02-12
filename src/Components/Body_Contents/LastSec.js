28ccba346f1de0c497410e91d6848d9c6c1916c7
sachin972 <190113027@hbtu.ac.in>
2023-02-12 05:44:06 +0530

    modified a bit

--- src/Components/Body_Contents/LastSec.js
+++ src/Components/Body_Contents/LastSec.js
@@ -1,20 +1,23 @@
 import React from "react";
+import bubble from './textures/bubble.jpg';
+import insertion from './textures/insertion.jpg';
+import selection from './textures/selection.jpg';
 
 function LastSec(){
 
 
-    const h1Style = {
-        fontFamily: ['Russo One', 'sans-serif'],
-        textAlign: 'center',
-        color: 'aliceblue'
-    }
+    // const h1Style = {
+    //     fontFamily: ['Russo One', 'sans-serif'],
+    //     textAlign: 'center',
+    //     color: 'aliceblue'
+    // }
 
     return (
         <section className="s1">
             <div className="main-container"><br />
                 <div data-aos="zoom-in" data-aos-duration="1500" className="post-wrapper">
                     <div className="post">
-                        <img className="thumbnail" src="textures/bubble.jpg" />
+                        <img className="thumbnail" src={bubble} />
                         <div className="post-preview">
                             <h6 className="post-title">Description</h6><br />
                             <p className="post-intro"> Bubble Sort is an iterative sorting algorithm that imitates the movement of bubbles in sparkling water..<br /><br />
@@ -29,7 +32,7 @@ function LastSec(){
                     </div>
     
                     <div className="post">
-                        <img className="thumbnail" src="textures/selection.jpg" />
+                        <img className="thumbnail" src={selection} />
                         <div className="post-preview">
                             <h6 className="post-title"><b>Description</b></h6><br />
                             <p className="post-intro">The selection sort algorithm sorts an array by repeatedly finding the minimum element from unsorted part and putting it at the beginning.                            <br /><br />
@@ -43,7 +46,7 @@ function LastSec(){
                     </div>
     
                     <div className="post">
-                        <img className="thumbnail" src="textures/insertion.jpg" />
+                        <img className="thumbnail" src={insertion} />
                         <div className="post-preview">
                             <h6 className="post-title">Description</h6><br />
                             <p className="post-intro">The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.<br /><br />
@@ -59,7 +62,7 @@ function LastSec(){
     
                 </div>
 
-                <h1 style={h1Style}>And, For No Reason!</h1>
+                {/* <h1 style={h1Style}>And, For No Reason!</h1> */}
             </div>
             
         </section>
